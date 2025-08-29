284d38e7100c5837eae6dbe974f4e7cd14b7edbd
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-08-29 13:15:06 +0530

    created base apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MainApp());
+    runApp(SinglesMatchApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SinglesMatchApp extends StatelessWidget {
+  const SinglesMatchApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Singles Match Scoring',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: PlayerRegistrationScreen(),
+    );
+  }
+}
+
+class PlayerRegistrationScreen extends StatelessWidget {
+  const PlayerRegistrationScreen({super.key});
+
+    @override
+    Widget build (BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(title: const Text("Player Registration")),
+            body: Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (_) => BattingOrderScreen()),
+                        );
+                    }, 
+                    child: const Text("Next: Batting Order"), 
+                ),
+            ),
+        );
+    }
+}
+
+class BattingOrderScreen extends StatelessWidget {
+  const BattingOrderScreen({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(title: const Text("Batting Order")),
+            body: Center(
+                child: ElevatedButton(onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => MatchScreen()),
+                    );
+                }, child: const Text("Next: Start Match")),
+            ),
+        );
+    }
+}
+
+class MatchScreen extends StatelessWidget {
+  const MatchScreen({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Match DashBoard")),
+        body: const Center( child: Text("Match in Progress")),
     );
   }
 }
